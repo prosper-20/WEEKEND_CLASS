666264d1795d9b4fe38dbeb716388439cab281b6
prosper <edwardprosper001@gmail.com>
2022-10-15 15:36:32 +0100

    initial

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-f = open("my_test.txt")
+# def my_country(name="Nigeria"):
+#     country = "I am from " + name
+#     return country
+
+
+# my_country()
+
+
+
+
+# x = list(map(int, input("Enter different numbers:").split()))
+
+# def duplicate(x):
+#     y = []
+#     for value in x:
+#         new = value * 2
+#         y.append(new)
+#     return y
+
+# print(duplicate(x))
+
+
+# def find_maximum(x):
+#     ans = max(x)
+#     return ans
+
+
+# print(find_maximum(x))
+
+
+y = input("Enter a chaacter string: ")
+def find_length(y):
+    return len(y)
+
+print(find_length(y))
+
+
+find_length("good")
